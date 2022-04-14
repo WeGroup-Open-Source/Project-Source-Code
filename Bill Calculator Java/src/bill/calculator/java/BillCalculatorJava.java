10fdb368f3907d0fc54169edaf49954d132c89a6
allenbagai <allenkamadje@gmail.com>
2022-04-14 03:26:21 +0200

    third and fourth commit

---
+++ Bill Calculator Java/src/bill/calculator/java/BillCalculatorJava.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bill.calculator.java;
+
+/**
+ *
+ * @author Kamadje Allen
+ */
+public class BillCalculatorJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("bill.calculator.java.BillCalculatorJava.main()");
+        // TODO code application logic here
+    }
+    
+}
---
+++ Calculator App Java/src/calculator/app/java/CalculatorAppJava.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculator.app.java;
+
+/**
+ *
+ * @author Kamadje Allen
+ */
+public class CalculatorAppJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("calculator.app.java.CalculatorAppJava.main()");
+    }
+    
+}
